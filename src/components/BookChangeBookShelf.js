8e61410ccf440629c987e4a1763d9c48ae162800
Ben Chan <benchanviolin@gmail.com>
2017-09-01 06:23:02 -0400

    type: feat
    Create BookChangeBookShelf.js and replace Book.js <select> with
    <BookChangeBookShelf>.  Pass options as PropTypes.array.isRequired from Book to
    BookChangeBookShelf for future extensibility.

---
+++ src/components/BookChangeBookShelf.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class BookChangeBookShelf extends React.Component {
+  static propTypes = {
+    options: PropTypes.array.isRequired
+  }
+
+  state = {
+
+  }
+  render() {
+    const { options } = this.props;
+
+    return (
+      <div className="book-shelf-changer">
+        <select>
+          {options.map((option) => (
+            <option value={option.value} disabled={option.hasOwnProperty('disabled') && option.disabled === true?'disabled':''}>{option.label}</option>
+          ))}          
+        </select>
+      </div>
+    )
+  }
+}
+
+export default BookChangeBookShelf
