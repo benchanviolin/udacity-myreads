25a5f3c4e2cde8c0f91256843a124193d0cfdb0e
Ben Chan <benchanviolin@gmail.com>
2017-09-01 10:57:05 -0400

    type: feat
    Move all API related calls to App.js and then pass props down to all
    children so that App is the source of truth for shelves, books, and
    options.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,62 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import * as BooksAPI from './utils/BooksAPI'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
 
 class BooksApp extends React.Component {
   state = {
-
+    books: [],
+    options: [
+      {
+        value: 'none',
+        disabled: true,
+        label: 'Move to...'
+      },
+      {
+        value: 'currentlyReading',
+        label: 'Currently Reading'
+      },
+      {
+        value: 'wantToRead',
+        label: 'Want to Read'
+      },
+      {
+        value: 'read',
+        label: 'Read'
+      },
+      {
+        value: 'none',
+        label: 'None'
+      }
+    ],
+    shelves: [
+      {
+        value: 'currentlyReading',
+        label: 'Currently Reading'
+      },
+      {
+        value: 'wantToRead',
+        label: 'Want to Read'
+      },
+      {
+        value: 'read',
+        label: 'Read'
+      }
+    ]
+  }
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
+  }
+  moveBookToAnotherShelf = (book, shelf) => {    
+    BooksAPI.update(book, shelf).then(() => {
+      BooksAPI.getAll().then((books) => {
+        this.setState({ books })
+      })
+    })
   }
 
   render() {
@@ -19,8 +69,12 @@ class BooksApp extends React.Component {
       <div className="app">
         <Route exact path='/' render={() => (
           <ListBooks
+            books={this.state.books}
+            shelves={this.state.shelves}
+            options={this.state.options}
             bookWidth={style.bookWidth}
             bookHeight={style.bookHeight}
+            moveBookToAnotherShelf={this.moveBookToAnotherShelf}
           />
         )}/>
       <Route path='/search-books' render={() => (
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,67 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import * as BooksAPI from './utils/BooksAPI'
 import Shelf from './components/Shelf'
 
 class ListBooks extends React.Component {
   static propTypes = {
+    books: PropTypes.array.isRequired,
+    options: PropTypes.array.isRequired,
     bookWidth: PropTypes.number.isRequired,
     bookHeight: PropTypes.number.isRequired,
-    onMoveBookToAnotherShelf: PropTypes.func
+    moveBookToAnotherShelf: PropTypes.func.isRequired
   }
 
   state = {
-    options: [
-      {
-        value: 'none',
-        disabled: true,
-        label: 'Move to...'
-      },
-      {
-        value: 'currentlyReading',
-        label: 'Currently Reading'
-      },
-      {
-        value: 'wantToRead',
-        label: 'Want to Read'
-      },
-      {
-        value: 'read',
-        label: 'Read'
-      },
-      {
-        value: 'none',
-        label: 'None'
-      }
-    ],
-    shelves: [
-      {
-        value: 'currentlyReading',
-        label: 'Currently Reading'
-      },
-      {
-        value: 'wantToRead',
-        label: 'Want to Read'
-      },
-      {
-        value: 'read',
-        label: 'Read'
-      }
-    ],
-    books: []
+
   }
   componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books })
-    })
-  }
-  moveBookToAnotherShelf = (book, shelf) => {
-    const oldShelf = book.shelf;
-    BooksAPI.update(book, shelf).then((books) => {
-      console.log(oldShelf);
 
-    })
   }
 
   render() {
@@ -72,16 +27,16 @@ class ListBooks extends React.Component {
         </div>
         <div className="list-books-content">
           <div>
-            {this.state.shelves.map((shelf, key) => (
+            {this.props.shelves.map((shelf, key) => (
               <Shelf
                 key={key}
                 id={shelf.value}
                 bookWidth={this.props.bookWidth}
                 bookHeight={this.props.bookHeight}
                 title={shelf.label}
-                books={this.state.books.filter(book => book.shelf === shelf.value)}
-                options={this.state.options}
-                moveBookToAnotherShelf={this.moveBookToAnotherShelf}
+                books={this.props.books.filter(book => book.shelf === shelf.value)}
+                options={this.props.options}
+                moveBookToAnotherShelf={this.props.moveBookToAnotherShelf}
               />
           ))}
           </div>
