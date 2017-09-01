b8bd85129f3e59c8777e09c8861a4adecf81883c
Ben Chan <benchanviolin@gmail.com>
2017-09-01 05:37:17 -0400

    type: feat
    Create ListBooks.js and move from App.js.  Add <BrowserRouter> to
    index.js.  Add two <Route> tags to App.js for SearchBooks and ListBooks.

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react'
 //import { Link } from 'react-router-dom';
 //import PropTypes from 'prop-types';
 
@@ -36,3 +36,5 @@ class SearchBooks extends React.Component {
     );
   }
 }
+
+export default SearchBooks
