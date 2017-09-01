b2978d1c7e4c68f896bb90a1b190a94b934f1d97
Ben Chan <benchanviolin@gmail.com>
2017-09-01 05:14:28 -0400

    type: feat
    Create SearchBooks.js with empty propTypes, state, and render.

---
+++ src/SearchBooks.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+//import { Link } from 'react-router-dom';
+//import PropTypes from 'prop-types';
+
+class SearchBooks extends React.Component {
+  static propTypes = {
+
+  };
+
+  state = {
+
+  };
+
+  render() {
+    return (
+
+    );
+  }
+}
